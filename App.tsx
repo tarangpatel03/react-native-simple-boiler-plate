e5fff8d87384836c8291a6e63d055438654780a3
Tarang Patel <tarangpatel133@gmail.com>
2026-08-20 19:10:45 +0530

    feat: integrate providers, navigation, and initial state in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,45 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import '@/locale/i18n';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { QueryClientProvider } from '@tanstack/react-query';
+import React, { useEffect } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+import { setupInterceptors } from '@/api/axios/interceptor';
+import { queryClient } from '@/api/query/queryClient';
+import { navigationRef } from '@/navigation/navigation.services';
+import { RootNavigator } from '@/navigation/RootNavigation';
+import { persistor, store } from '@/store/store';
+
+export const App = (): React.JSX.Element => {
+  useEffect(() => {
+    setupInterceptors();
+  }, []);
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <QueryClientProvider client={queryClient}>
+            <NavigationContainer ref={navigationRef}>
+              <RootNavigator />
+            </NavigationContainer>
+          </QueryClientProvider>
+        </PersistGate>
+      </Provider>
     </SafeAreaProvider>
   );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+};
 
 export default App;
